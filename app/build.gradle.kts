5c9b3da8d7e4b82c3e6d4d8ccb23eba33dfaba58
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-16 12:30:27 +0700

    udate (application) build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("java")
+    application
 }
-
-group = "hexlet-code"
+group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 tasks.test {
