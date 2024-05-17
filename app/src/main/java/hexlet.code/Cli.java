20ef46aa562329f9130cd300e37a03d3a3647d9a
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-17 13:13:10 +0700

    update app/src/main/java/hexlet.code/Cli.java

--- app/src/main/java/hexlet.code/Cli.java
+++ app/src/main/java/hexlet.code/Cli.java
@@ -2,7 +2,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void greetings() {
+    public static void greeting() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
