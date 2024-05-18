5f8e38cc82508cf469e33b8fc31215082805e501
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-18 15:46:14 +0700

    update Even.java

--- app/src/main/java/hexlet.code/Even.java
+++ app/src/main/java/hexlet.code/Even.java
@@ -1,32 +1,28 @@
 package hexlet.code;
 
-import java.util.Scanner;
-
 public class Even {
     public static void gameEven() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String name = sc.nextLine();
-        System.out.println("Hello, " + name + "!");
+        Engine engine = new Engine();
+        engine.greeting();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         int count = 0;
         while (count < 3) {
             int randomNumber = (int) (Math.random() * (200 + 1)) + 1;
-            System.out.println("Question: " + randomNumber);
-            System.out.print("Your answer: ");
-            String answer = sc.nextLine();
-            if ((randomNumber % 2 == 0 && answer.equals("yes"))
-                    || (randomNumber % 2 != 0 && answer.equals("no"))) {
+            engine.questionAndAnswer(randomNumber);
+            if ((randomNumber % 2 == 0 && engine.getAnswer().equals("yes"))
+                    || (randomNumber % 2 != 0 && engine.getAnswer().equals("no"))) {
                 System.out.println("Correct!");
                 count++;
-            } else if (!(answer.equals("yes")) || !(answer.equals("no"))) {
+            } else if (engine.getAnswer().equals("yes") && randomNumber % 2 != 0) {
                 System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.\n"
-                        + "Let's try again, " + name);
+                        + "Let's try again, " + engine.getName());
                 break;
+            } else if (engine.getAnswer().equals("no") && randomNumber % 2 == 0) {
+                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.\n"
+                        + "Let's try again, " + engine.getName());
             }
             if (count == 3) {
-                System.out.println("Congratulations, " + name);
+                System.out.println("Congratulations, " + engine.getName());
             }
         }
     }
