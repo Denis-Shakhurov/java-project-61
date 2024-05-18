5610a6553fefe1dbfc84768953ca4f20b7c33ea3
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-18 15:45:26 +0700

    update App.java

--- app/src/main/java/hexlet.code/App.java
+++ app/src/main/java/hexlet.code/App.java
@@ -3,17 +3,20 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class App {
+    public static Engine engine = new Engine();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter the game number and press Enter.\n"
                 + "1 - Greet\n"
                 + "2 - Even\n"
+                + "3 - Calc\n"
                 + "0 - Exit\n"
                 + "Your choice: ");
         int numberGame = sc.nextInt();
         switch (numberGame) {
-            case 1 : Cli.greeting(); break;
+            case 1 : engine.greeting(); break;
             case 2 : Even.gameEven(); break;
+            case 3 : Calc.gameCalc(); break;
             default :
                 System.out.println("Enter number of list");
         }
