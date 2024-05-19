ed58906bf2056cfcf4813964a678e112334c3d4a
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-19 09:07:19 +0700

    update Engine.java

--- app/src/main/java/hexlet.code/Engine.java
+++ app/src/main/java/hexlet.code/Engine.java
@@ -19,6 +19,11 @@ public class Engine {
         System.out.print("Your answer: ");
         answer = sc.nextLine();
     }
+    public void questionAndAnswer(int number1, int number2) {
+        System.out.println("Question: " + number1 + " " + number2);
+        System.out.print("Your answer: ");
+        answer = sc.nextLine();
+    }
     public void questionAndAnswer(String math, int number1, int number2) {
         System.out.println("Question: " + number1 + " " + math + " " + number2);
         System.out.print("Your answer: ");
@@ -29,5 +34,16 @@ public class Engine {
         System.out.print("May I have your name? ");
         name = sc.nextLine();
         System.out.println("Hello, " + getName() + "!");
+    }
+    public void printGameWin() {
+        System.out.println("Congratulations, " + getName() + "!");
+    }
+    public void printGameLose(String text) {
+        System.out.println("'" + getAnswer() + "' is wrong answer ;(. Correct answer was '"
+                + text + "'.\n" + "Let's try again, " + getName() + "!");
+    }
+    public void printGameLose(int result) {
+        System.out.println("'" + getAnswer() + "' is wrong answer ;(. Correct answer was '"
+                + result + "'.\n" + "Let's try again, " + getName() + "!");
     }
 }
