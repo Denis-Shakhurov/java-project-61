6fb9aa1a2586a34ef6feab89f7d6c63914456703
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-26 12:17:43 +0700

    refactoring Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,26 +1,38 @@
 package hexlet.code.games;
 
-import hexlet.code.App;
 import hexlet.code.Engine;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Prime {
+    private static final int NUMBER_OF_GAMES = 3;
     private static final int INDEX = 100;
+    private static final int INDEX_1 = 3;
+    public static boolean checkNumber(int number) {
+        boolean check = true;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
     public static void gamePrime() {
         Engine.greeting();
         System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-        int count = 0;
-        while (count < App.ROUND_OF_GAME) {
-            int number = (int) (Math.random() * INDEX);
-            Engine.questionAndAnswer(number);
-            String answer = Engine.getAnswer();
-            if (Engine.enginePrime(number, answer)) {
-                count++;
+        Engine.engineGame(getQuestionsAndAnswers());
+    }
+    public static Map<String, String> getQuestionsAndAnswers() {
+        Map<String, String> maps = new HashMap<>();
+        for (int i = 0; i < NUMBER_OF_GAMES; i++) {
+            int number = (int) (INDEX_1 + Math.random() * INDEX);
+            if (checkNumber(number)) {
+                maps.put(String.valueOf(number), "yes");
             } else {
-                break;
-            }
-            if (count == App.ROUND_OF_GAME) {
-                Engine.printGameWin();
+                maps.put(String.valueOf(number), "no");
             }
         }
+        return maps;
     }
 }
