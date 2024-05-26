d03df1085d0e0f49064b9b6b9de0cbbd0a3b1831
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-26 12:17:02 +0700

    refactoring Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,26 +1,31 @@
 package hexlet.code.games;
 
-import hexlet.code.App;
 import hexlet.code.Engine;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Even {
+    private static final int NUMBER_OF_GAMES = 3;
     private static final int INDEX = 200;
+
     public static void gameEven() {
         Engine.greeting();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        int count = 0;
-        while (count < App.ROUND_OF_GAME) {
-            int randomNumber = (int) (1 + Math.random() * INDEX);
-            Engine.questionAndAnswer(randomNumber);
-            String answer = Engine.getAnswer();
-            if (Engine.engineEven(randomNumber, answer)) {
-                count++;
+        Engine.engineGame(getQuestionsAndAnswers());
+    }
+    public static Map<String, String> getQuestionsAndAnswers() {
+        Map<String, String> maps = new HashMap<>();
+        for (int i = 0; i < NUMBER_OF_GAMES; i++) {
+            int number = (int) (1 + Math.random() * INDEX);
+            if (isEven(number)) {
+                maps.put(String.valueOf(number), "yes");
             } else {
-                break;
-            }
-            if (count == App.ROUND_OF_GAME) {
-                Engine.printGameWin();
+                maps.put(String.valueOf(number), "no");
             }
         }
+        return maps;
+    }
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
