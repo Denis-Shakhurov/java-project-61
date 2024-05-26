fd4c55959548d2c6a8f9430837c95c2a1c31c631
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-26 12:16:40 +0700

    refactoring Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,28 +1,44 @@
 package hexlet.code.games;
 
-import hexlet.code.App;
 import hexlet.code.Engine;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Calc {
+    private static final int NUMBER_OF_GAMES = 3;
     private static final int INDEX_NUMBER = 10;
-    private static final int INDEX_MATH_OPERATION = 3;
+    private static final int INDEX = 3;
     public static void gameCalc() {
 
         Engine.greeting();
         System.out.println("What is the result of the expression?");
-        int count = 0;
-        while (count < App.ROUND_OF_GAME) {
-            int number1 = (int) (Math.random() * INDEX_NUMBER);
-            int number2 = (int) (Math.random() * INDEX_NUMBER);
-            int index = (int) (Math.random() * INDEX_MATH_OPERATION);
-            if (Engine.engineCalc(number1, number2, index)) {
-                count++;
-            } else {
-                break;
+        Engine.engineGame(getQuestionsAndAnswers());
+    }
+    public static Map<String, String> getQuestionsAndAnswers() {
+        Map<String, String> maps = new HashMap<>();
+        for (int i = 0; i < NUMBER_OF_GAMES; i++) {
+            int number1 = (int) ((Math.random() * INDEX_NUMBER + 1) + 1);
+            int number2 = (int) ((Math.random() * INDEX_NUMBER + 1) + 1);
+            int index = (int) (Math.random() * INDEX);
+            if (index == 0) {
+                int resultWork = number1 * number2;
+                String questionWork = number1 + " * " + number2;
+                String answerWork = String.valueOf(resultWork);
+                maps.put(questionWork, answerWork);
             }
-            if (count == App.ROUND_OF_GAME) {
-                Engine.printGameWin();
+            if (index == 1) {
+                int resultSum = number1 + number2;
+                String questionSum = number1 + " + " + number2;
+                String answerSum = String.valueOf(resultSum);
+                maps.put(questionSum, answerSum);
+            }
+            if (index == 2) {
+                int resultSub = number1 - number2;
+                String questionSub = number1 + " - " + number2;
+                String answerSub = String.valueOf(resultSub);
+                maps.put(questionSub, answerSub);
             }
         }
+        return maps;
     }
 }
