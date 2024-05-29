c97e32f4988ee3df68f35e4903ed23ee3f645ec8
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-29 12:01:32 +0700

    refactoring GCD.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -1,30 +1,33 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-import java.util.HashMap;
+import hexlet.code.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class GCD {
-    private static final int NUMBER_OF_GAMES = 3;
-    private static final int INDEX = 100;
+    private static final int NUMBER_OF_ROUNDS = 3;
+
     public static int gcd(int a, int b) {
         return (a % b == 0) ? Math.abs(b) : gcd(b, a % b);
     }
+
     public static void gameGsd() {
-        Engine.greeting();
-        System.out.println("Find the greatest common divisor of given numbers.");
-        Engine.engineGame(getQuestionsAndAnswers());
+        Engine.engineGame(getQuestionsAndAnswers(), "GSD");
     }
-    public static Map<String, String> getQuestionsAndAnswers() {
-        Map<String, String> maps = new HashMap<>();
-        for (int i = 0; i < NUMBER_OF_GAMES; i++) {
-            int number1 = (int) (Math.random() * (INDEX + 1)) + 1;
-            int number2 = (int) (Math.random() * (INDEX + 1)) + 1;
+
+    public static List<Map<String, String>> getQuestionsAndAnswers() {
+        List<Map<String, String>> list = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
+            int number1 = Utils.getRandomInt(0, 100);
+            int number2 = Utils.getRandomInt(0, 100);
             int result = gcd(number1, number2);
             String question = number1 + " " + number2;
             String answer = String.valueOf(result);
-            maps.put(question, answer);
+            list.add(Map.of(question, answer));
         }
-        return maps;
+        return list;
     }
 }
