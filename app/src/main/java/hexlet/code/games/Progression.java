7c4bd1e2c6a302a2073d3644606691ce8881bb11
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-29 12:02:07 +0700

    refactoring Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,53 +1,47 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-import java.util.HashMap;
+import hexlet.code.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Progression {
-    private static final int NUMBER_OF_GAMES = 3;
+    private static final int NUMBER_OF_ROUNDS = 3;
     private static final int LENGTH = 10;
-    private static final int INDEX_START = 100;
-    private static final int INDEX = 5;
+
     public static void gameProgression() {
-        Engine.greeting();
-        System.out.println("What number is missing in the progression?");
-        Engine.engineGame(getQuestionsAndAnswers());
+        Engine.engineGame(getQuestionsAndAnswers(), "Progression");
     }
-    public static int[] arrayInt() {
-        //int length = (int) (5 + Math.random() * 5);
-        int start = (int) (Math.random() * INDEX_START);
-        int index = (int) (1 + Math.random() * INDEX);
-        int[] array = new int[LENGTH];
-        array[0] = start;
+
+    public static String[] progression() {
+        int start = Utils.getRandomInt(0, 100);
+        int index = Utils.getRandomInt(1, 5);
+        String[] array = new String[LENGTH];
+        array[0] = String.valueOf(start);
         for (int i = 1; i < array.length; i++) {
-            array[i] = array[i - 1] + index;
+            array[i] = String.valueOf(Integer.parseInt(array[i - 1]) + index);
         }
         return array;
     }
-    public static String arrayWithEllipsis(int index, int[] array) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < array.length; i++) {
-            if (i == index) {
-                sb.append("..");
-                sb.append(" ");
-            } else {
-                sb.append(array[i]);
-                sb.append(" ");
-            }
-        }
-        return sb.toString().trim();
+
+    public static String arrayWithEllipsis(int index, String[] array) {
+        array[index] = "..";
+        String res = String.join(" ", array);
+        return res;
     }
-    public static Map<String, String> getQuestionsAndAnswers() {
-        Map<String, String> maps = new HashMap<>();
-        for (int i = 0; i < NUMBER_OF_GAMES; i++) {
-            int[] array = arrayInt();
-            int index = (int) (1 + Math.random() * (array.length - 1));
-            int result = array[index];
+
+    public static List<Map<String, String>> getQuestionsAndAnswers() {
+        List<Map<String, String>> list = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
+            String[] array = progression();
+            int index = Utils.getRandomInt(1, LENGTH - 1);
+            String result = array[index];
             String question = arrayWithEllipsis(index, array);
-            String answer = String.valueOf(result);
-            maps.put(question, answer);
+            String answer = result;
+            list.add(Map.of(question, answer));
         }
-        return maps;
+        return list;
     }
 }
