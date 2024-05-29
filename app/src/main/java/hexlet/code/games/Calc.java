74db45d58ae247f97539b4a05370d97ec0fb57b9
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-29 12:00:51 +0700

    refactoring Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,44 +1,44 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-import java.util.HashMap;
+import hexlet.code.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Calc {
-    private static final int NUMBER_OF_GAMES = 3;
-    private static final int INDEX_NUMBER = 10;
-    private static final int INDEX = 3;
-    public static void gameCalc() {
+    private static final int NUMBER_OF_ROUNDS = 3;
 
-        Engine.greeting();
-        System.out.println("What is the result of the expression?");
-        Engine.engineGame(getQuestionsAndAnswers());
+    public static void gameCalc() {
+        Engine.engineGame(getQuestionsAndAnswers(), "Calc");
     }
-    public static Map<String, String> getQuestionsAndAnswers() {
-        Map<String, String> maps = new HashMap<>();
-        for (int i = 0; i < NUMBER_OF_GAMES; i++) {
-            int number1 = (int) ((Math.random() * INDEX_NUMBER + 1) + 1);
-            int number2 = (int) ((Math.random() * INDEX_NUMBER + 1) + 1);
-            int index = (int) (Math.random() * INDEX);
+
+    public static List<Map<String, String>> getQuestionsAndAnswers() {
+        List<Map<String, String>> list = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
+            int number1 = Utils.getRandomInt(0, 10);
+            int number2 = Utils.getRandomInt(0, 10);
+            int index = Utils.getRandomInt(0, 2);
             if (index == 0) {
                 int resultWork = number1 * number2;
                 String questionWork = number1 + " * " + number2;
                 String answerWork = String.valueOf(resultWork);
-                maps.put(questionWork, answerWork);
+                list.add(Map.of(questionWork, answerWork));
             }
             if (index == 1) {
                 int resultSum = number1 + number2;
                 String questionSum = number1 + " + " + number2;
                 String answerSum = String.valueOf(resultSum);
-                maps.put(questionSum, answerSum);
+                list.add(Map.of(questionSum, answerSum));
             }
             if (index == 2) {
                 int resultSub = number1 - number2;
                 String questionSub = number1 + " - " + number2;
                 String answerSub = String.valueOf(resultSub);
-                maps.put(questionSub, answerSub);
+                list.add(Map.of(questionSub, answerSub));
             }
         }
-        return maps;
+        return list;
     }
 }
