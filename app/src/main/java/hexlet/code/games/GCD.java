b6ae95770f1f063836a0789c8fadbe926cd77643
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-29 12:34:10 +0700

    refactoring GCD.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 public class GCD {
     private static final int NUMBER_OF_ROUNDS = 3;
+    private static final int MAX_INT = 100;
 
     public static int gcd(int a, int b) {
         return (a % b == 0) ? Math.abs(b) : gcd(b, a % b);
@@ -21,8 +22,8 @@ public class GCD {
     public static List<Map<String, String>> getQuestionsAndAnswers() {
         List<Map<String, String>> list = new ArrayList<>();
         for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
-            int number1 = Utils.getRandomInt(0, 100);
-            int number2 = Utils.getRandomInt(0, 100);
+            int number1 = Utils.getRandomInt(0, MAX_INT);
+            int number2 = Utils.getRandomInt(0, MAX_INT);
             int result = gcd(number1, number2);
             String question = number1 + " " + number2;
             String answer = String.valueOf(result);
