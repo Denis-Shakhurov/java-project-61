8b80cb606e9bab1556a9f21ae471564c200ea8fa
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-31 16:45:38 +0700

    refactoring Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,30 +1,20 @@
 package hexlet.code;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Engine {
-    private static Scanner sc = new Scanner(System.in);
-    private static String answer = "";
-    private static String name = "";
-    private static Map<String, String> mapGame = new HashMap<>();
 
-    static {
-        mapGame.put("Even", "Answer 'yes' if the number is even, otherwise answer 'no'.");
-        mapGame.put("Calc", "What is the result of the expression?");
-        mapGame.put("GCD", "Find the greatest common divisor of given numbers.");
-        mapGame.put("Progression", "What number is missing in the progression?");
-        mapGame.put("Prime", "Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-    }
-
-    public static void engineGame(List<Map<String, String>> list, String nameGame) {
+    public static void engineGame(List<Map<String, String>> list, String ruleGame) {
+        Scanner sc = new Scanner(System.in);
+        String answer = "";
+        String name = "";
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         name = sc.nextLine();
         System.out.println("Hello, " + name + "!");
-        System.out.println(mapGame.get(nameGame));
+        System.out.println(ruleGame);
 
         int count = 0;
 
