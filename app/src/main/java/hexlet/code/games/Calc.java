4bec61c141d9a6fa73ace03a58e46b68f059f532
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-05-31 16:45:58 +0700

    refactoring Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -12,34 +12,33 @@ public class Calc {
     private static final int MAX_INT = 10;
 
     public static void gameCalc() {
-        Engine.engineGame(getQuestionsAndAnswers(), "Calc");
+        String ruleCalc = "What is the result of the expression?";
+        Engine.engineGame(getQuestionsAndAnswers(), ruleCalc);
     }
 
     public static List<Map<String, String>> getQuestionsAndAnswers() {
         List<Map<String, String>> list = new ArrayList<>();
+        String[] maths = {"+", "-", "*"};
         for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
             int number1 = Utils.getRandomInt(0, MAX_INT);
             int number2 = Utils.getRandomInt(0, MAX_INT);
             int index = Utils.getRandomInt(0, 2);
-            if (index == 0) {
-                int resultWork = number1 * number2;
-                String questionWork = number1 + " * " + number2;
-                String answerWork = String.valueOf(resultWork);
-                list.add(Map.of(questionWork, answerWork));
-            }
-            if (index == 1) {
-                int resultSum = number1 + number2;
-                String questionSum = number1 + " + " + number2;
-                String answerSum = String.valueOf(resultSum);
-                list.add(Map.of(questionSum, answerSum));
-            }
-            if (index == 2) {
-                int resultSub = number1 - number2;
-                String questionSub = number1 + " - " + number2;
-                String answerSub = String.valueOf(resultSub);
-                list.add(Map.of(questionSub, answerSub));
-            }
+            String math = maths[index];
+            String question = number1 + " " + math + " " + number2;
+            String answer = mathOperation(number1, number2, math);
+            list.add(Map.of(question, answer));
         }
         return list;
+    }
+    public static String mathOperation(int number1, int number2, String math) {
+        String result = "";
+        if (math.equals("+")) {
+            result = String.valueOf(number1 + number2);
+        } else if (math.equals("-")) {
+            result = String.valueOf(number1 - number2);
+        } else {
+            result = String.valueOf(number1 * number2);
+        }
+        return result;
     }
 }
