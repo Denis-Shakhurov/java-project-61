82942b5ce1ecd7611e17e62544d607755a179b02
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-06-05 09:59:29 +0700

    refactoring Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -28,15 +28,15 @@ public class Prime {
     }
 
     public static List<Map<String, String>> getQuestionsAndAnswers() {
-        List<Map<String, String>> list = new ArrayList<>();
+        List<Map<String, String>> questionsAndAnswers = new ArrayList<>();
         for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
             int number = Utils.getRandomInt(1, MAX_INT);
             if (checkNumber(number)) {
-                list.add(Map.of(String.valueOf(number), "yes"));
+                questionsAndAnswers.add(Map.of(String.valueOf(number), "yes"));
             } else {
-                list.add(Map.of(String.valueOf(number), "no"));
+                questionsAndAnswers.add(Map.of(String.valueOf(number), "no"));
             }
         }
-        return list;
+        return questionsAndAnswers;
     }
 }
